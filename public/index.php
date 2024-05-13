5603414fea06f5a702e2bef4c758682b673e0ced
ozeemm <77226278+ozeemm@users.noreply.github.com>
2024-05-13 12:33:28 +0800

    Task 4.6: Переписали 4.4 под Twig

--- public/index.php
+++ public/index.php
@@ -1,38 +1,63 @@
 <?php
-    // Подключаем пакеты
-    require_once '../vendor/autoload.php';
+        // Подключаем пакеты
+        require_once '../vendor/autoload.php';
 
-    // Создаем загрузчик шаблонов, и указываем папку с шаблонами
-    // только слеш вместо точек
-    $loader = new \Twig\Loader\FilesystemLoader('../views');
+        // Создаем загрузчик шаблонов, и указываем папку с шаблонами
+        // только слеш вместо точек
+        $loader = new \Twig\Loader\FilesystemLoader('../views');
+    
+        // Экземпляр Twig с помощью которого будет рендерить
+        $twig = new \Twig\Environment($loader);
+        $url = $_SERVER["REQUEST_URI"];
+        
+        $context = [];
+        $template = "";
+        $title = "";
 
-    // Экземпляр Twig с помощью которого будет рендерить
-    $twig = new \Twig\Environment($loader);
+        $menu = [["title" => "Главная", "url" => "/"],
+                ["title" => "Фрай", "url" => "/Fry"], 
+                ["title" => "Бендер", "url" => "/Bender"]];
 
-    $url = $_SERVER["REQUEST_URI"];
-    $title = "";
-    $template = "";
-    $context = []; // Пустой словарик
-    $menu = [["title" => "Главная", "url" => "/"],
-            ["title" => "Древо", "url" => "/tree"],
-            ["title" => "Ручеёк", "url" => "/water"]];
+        $nav_menu = [["name" => "Фрай", "url" => "/Fry"],
+                    ["name" => "Бендер", "url" => "/Bender"]];
 
-    if($url == "/"){
-        $title = "Главная";
-        $template = "main.twig";
-    }
-    elseif($url == "/tree"){
-        $title = "Древо";
-        $template = "base_image.twig";
-        $context["image"] = "/images/tree.jpg";
-    }
-    elseif($url == "/water"){
-        $title = "Ручеёк";
-        $template = "base_image.twig";
-        $context["image"] = "/images/water.jpg";
-    }
-    $context["menu"] = $menu;
-    $context["title"] = $title;
-
-    echo $twig->render($template, $context);
+        if($url == "/"){
+            $template = "main.twig";
+            $title = "Главная";
+        }   
+        elseif(preg_match("#^/Fry#", $url)){
+            $template = "object.twig";
+            $title = "Фрай";
+            $context["url"] = "/Fry";
+            $context["image"] = "/images/Fry.jpg";
+            $context["description"] = "Родился 14 августа 1974 года.\nЗаморозил себя в криогенной камере за несколько секунд до наступления 2000 года.\nРазморозился в конце 2999 года, и в дальнейшем стал курьером в компании «Межпланетный Экспресс».";
+            if(preg_match("#^/Fry/image#", $url)){
+                $context["is_image"] = true;
+                $template = "image.twig";
+            }
+            elseif(preg_match("#^/Fry/info#", $url)){
+                $context["is_info"] = true;
+                $template = "info.twig";
+            }
+        }
+        else if(preg_match("#^/Bender#", $url)){
+            $template = "object.twig";
+            $title = "Бендер";
+            $context["url"] = "/Bender";
+            $context["image"] = "/images/Bender.jpg";
+            $context["description"] = "Был сделан в Мексике в 2997 году. Имеет серийный номер 2716057.\nПьет большое количество алкоголя, чтобы подзарядить свои топливные элементы.\nАвантюрист, любит курить сигары.\nВ настоящее время живет в квартире с Фраем.";
+            if(preg_match("#^/Bender/image#", $url)){
+                $context["is_image"] = true;
+                $template = "image.twig";
+            }
+            elseif(preg_match("#^/Bender/info#", $url)){
+                $context["is_info"] = true;
+                $template = "info.twig";
+            }
+        }
+        $context["menu"] = $menu;
+        $context["nav_menu"] = $nav_menu;
+        $context["title"] = $title;
+        
+        echo $twig->render($template, $context);
 ?>
